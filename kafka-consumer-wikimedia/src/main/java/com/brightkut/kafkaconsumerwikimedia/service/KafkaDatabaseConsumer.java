ec6418cd2d0b505e944bb6f4cc48f60878c070e2
brightkut <dsorn2@gmail.com>
2024-11-22 06:30:31 +0700

    master
    init project

---
+++ kafka-consumer-wikimedia/src/main/java/com/brightkut/kafkaconsumerwikimedia/service/KafkaDatabaseConsumer.java
@@ -0,0 +1,16 @@
+package com.brightkut.kafkaconsumerwikimedia.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaDatabaseConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+
+    @KafkaListener(topics = "wikimedia_recentchange", groupId = "group1")
+    public void consume(String eventMessage){
+        LOGGER.info("Event message received -> {}", eventMessage);
+    }
+}
